239e9e2909cdd4a216f3d4666eaedc3e9444f550
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-03-15 11:48:02 +0900

    js 기초 - 산술연산자

---
+++ 1. 자바스크립트 기초/2. 산술연산자.js
@@ -0,0 +1,25 @@
+
+//나머지 연산
+console.log(27 % 5);
+
+console.log(4 % 10);
+
+//거듭제곱 연산
+var num = 2;
+console.log(num ** 3);
+
+//증감연산자
+var x = 3;
+x++;
+console.log(x);
+
+// 전위 연산, 후위 연산
+var n1 = 10;
+var n2 = n1++;
+
+console.log(`n1: ${n1}, n2: ${n2}`);
+
+var n3 = 10;
+var n4 = ++n3;
+
+console.log(`n1: ${n3}, n2: ${n4}`);
